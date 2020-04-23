a3ee91e83100b9a0399377d61269b63e48b265e5
Kaspar Guggenbühl <k.guggenbuehl@backslash.ch>
2020-04-23 08:52:26 +0200

    cleaning code

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,6 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 
-/* change your directory and settings here */
-const settings = {
-    publicDir: '.',
-    cssDir: 'css'
-};
-
 /**
  * serve task, will launch browserSync and launch index.html files
  * and watch the changes for html and css files.
@@ -18,20 +12,15 @@ gulp.task('serve', function() {
      * Launch BrowserSync from publicDir
      */
     browserSync.init({
-        server: settings.publicDir,
-        index: "default.html", /* could be changed to "template.html" */
-        browser: "google chrome"
+        // watch: true,
+        server: true,
+        files: ["css/*.css", "js/*.js", "*.html"],
+        index: "default.html",
+        browser: "google chrome",
+        notify: false,
+        injectChanges: false,
     });
-
-    /**
-     * watch for changes in html and css files
-     */
-    gulp.watch([
-        settings.cssDir + "/**/*.css",
-        settings.publicDir + "/**/*.html",
-        ])
-    .on('change', browserSync.reload);
 
 });
 
-gulp.task('default', gulp.parallel('serve'));
+gulp.task('default', gulp.series('serve'));
