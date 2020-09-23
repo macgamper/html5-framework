a12dd19468f0bbb6d4ee696cbfc346665441fa7f
Mischa Sameli <m.sameli@backslash.ch>
2020-09-23 16:54:19 +0200

    Update custom.js
    Changed naming of selectors

--- js/custom.js
+++ js/custom.js
@@ -12,46 +12,47 @@ $(document).ready(function(){
     }
 
     // toggles menu
+    const navToggleClass = 'hamburger--active';
     // close menu if closeMenu === true
     function toggleMenu(container, list, btn, closeMenu) {
         if (closeMenu) {
             $('html').removeClass('js-mobilenav-visible');
             $(btn).removeClass(navToggleClass);
             $(container).slideUp(200);
-            $(navBackgroundClass).fadeOut(200);
+            $(selectors.navBackground).fadeOut(200);
         } else {
             $('html').toggleClass('js-mobilenav-visible');
             $(btn).toggleClass(navToggleClass);
-            $(navBackgroundClass).fadeToggle(200);
+            $(selectors.navBackground).fadeToggle(200);
             $(container).slideToggle(200);
         }
         toggleAriaExpanded(btn, closeMenu);
     }
 
     // hamburger & mobilenav
-    const navButtonClass = '.js-hamburger';
-    const navBackgroundClass = '.js-mobilenav__bg';
-    const navContainerClass = '.mobilenav__container';
-    const navListClass = '.mobilenav-box > ul';
-    const navToggleClass = 'hamburger--active';
-    const navSkiplink = 'a[href="#mainnav"]';
-    let navIsOpen = false;
-
-    $(navButtonClass + ', ' + navBackgroundClass).on('click', function(e) {
+    const selectors = {
+        navButton : '.js-hamburger',
+        navBackground : '.js-mobilenav__bg',
+        navContainer : '.mobilenav__container',
+        navList : '.mobilenav-box > ul',
+        navSkiplink : 'a[href="#mainnav"]'
+    };
+    
+    $(selectors.navButton + ', ' + selectors.navBackground).on('click', function(e) {
         // toggle menu
-        toggleMenu(navContainerClass, navListClass, navButtonClass);
+        toggleMenu(selectors.navContainer, selectors.navList, selectors.navButton);
 
         // close by pressing ESC
         // loop through menu by tab
-        $(navContainerClass + ', ' + navButtonClass).keydown(function(e) {
+        $(selectors.navContainer + ', ' + selectors.navButton).keydown(function(e) {
             if (e.keyCode === 27) {
-                toggleMenu(navContainerClass, navListClass, navButtonClass, true);
-                $(navButtonClass).focus();
+                toggleMenu(selectors.navContainer, selectors.navList, selectors.navButton, true);
+                $(selectors.navButton).focus();
                 return false;
             } else if (e.keyCode === 9) {
-                if (e.target === $(navListClass).find('li:last > *:last-child')[0]) {
+                if (e.target === $(selectors.navList).find('li:last > *:last-child')[0]) {
                     e.preventDefault;
-                    $(navButtonClass).focus();
+                    $(selectors.navButton).focus();
                     return false;
                 }
             }
@@ -59,18 +60,18 @@ $(document).ready(function(){
     });
 
     // change skiplink-href for navigation, when mobilenav is visible
-    $(navSkiplink).on('click', function(e){
-        if ($(navButtonClass).is(":visible")){
+    $(selectors.navSkiplink).on('click', function(e){
+        if ($(selectors.navButton).is(":visible")){
             e.preventDefault;
-            $(navButtonClass).click();
-            $(navListClass).find('>li:first-child()>a').focus();
+            $(selectors.navButton).click();
+            $(selectors.navList).find('>li:first-child()>a').focus();
             return false;
         }
     });
 
     // ************************************
     // presentation-tabellen mit entprechender ARIA-Role versehen
-    $('table.presentation').attr("role","presentation");
+    $('table.presentation').attr('role','presentation');
 
     // ************************************
     // keine icons für verlinkte bilder
@@ -79,16 +80,16 @@ $(document).ready(function(){
     // ************************************
     // touch-friendly dropdown-menus
     function isTouchDevice(){
-      return typeof window.ontouchstart !== 'undefined';
+        return typeof window.ontouchstart !== 'undefined';
     }
     /* If mobile browser, prevent click on parent nav item from redirecting to URL */
     if(isTouchDevice()) {
         // 1st click, add "clicked" class, preventing the location change. 2nd click will go through.
-        jQuery(".mainnav .folder > a").click(function(event) {
+        $('.mainnav .folder > a').click(function(event) {
             // Perform a reset - Remove the "clicked" class on all other menu items
-            jQuery(".mainnav .folder > a").not(this).removeClass("clicked");
-            jQuery(this).toggleClass("clicked");
-            if (jQuery(this).hasClass("clicked")) {
+            $('.mainnav .folder > a').not(this).removeClass('clicked');
+            $(this).toggleClass('clicked');
+            if ($(this).hasClass('clicked')) {
                 event.preventDefault();
             }
         });
